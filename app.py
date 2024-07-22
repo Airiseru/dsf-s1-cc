480b366f4cb37f5dc280fb783d211a160ea7a7d3
unknown <nanideech@gmail.com>
2024-07-22 21:56:02 +0800

    add plots in jupyter notebook and streamlit app

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ COLOR_YELLOW = "#f5ba01"
 COLOR_LIGHTGREEN = "#9acba2"
 COLOR_GREEN = "#41916c"
 COLOR_BLUE = "#213875"
+COLORS = [COLOR_RED, COLOR_BLUE, COLOR_GREEN, COLOR_YELLOW, COLOR_LIGHTGREEN]
 PROJ_TITLE = "An Analysis on AAC's Customers and their Spending Behaviors"
 
 # Loading the datasets
@@ -159,7 +160,7 @@ elif my_page == "Methodology":
     st.subheader("Methodology")
     st.markdown("To learn more about the dataset, this page will showcase the data preprocessing done, explore the data through graphs and charts, and showcase how the clustering was done using KMeans. To view the whole code, you can check the [Jupyter Notebook](https://github.com/Airiseru/dsf-s1-cc/blob/main/cc-project-nb.ipynb) for more.")
     st.write("<br>", unsafe_allow_html=True)
-    
+
     with st.expander("⚙️ **Data Preprocessing**", expanded=True):
         initial_df = pd.read_csv('cc_dirty.csv')
 
@@ -169,7 +170,73 @@ elif my_page == "Methodology":
         st.markdown("\nThus from the initial 100,000 transactions, it was down to 92,432 transactions after cleaning.")
     
     with st.expander("🔍 **Exploratory Data Analysis**", expanded=True):
-        st.write("wow!")
+        st.write("To do an initial exploration of the data, the following were looked at:\n1. Timeline of transaction\n2. Number of account holders (customers)\n3. Gender distribution\n4. Age distribution (based on generation)\n5. City distribution\n6. Categories with the highest number of transactions\n7. Categories with the highest amount spent")
+        st.markdown("<h4>Transaction Timeline and Number of Account Holders</h4>", unsafe_allow_html=True)
+        # insert the timeline and number of account holders
+
+        # Gender Distribution Plot
+        st.markdown("<h4>Gender Distribution</h4>", unsafe_allow_html=True)
+        gender_count = unique_holders['gender'].value_counts().to_frame(name="count").reset_index()
+        gender_fig = px.bar(gender_count, x='gender', y='count', color='gender')
+        st.plotly_chart(gender_fig, key='gender_distri_plot')
+        st.write("This plot tells us that the majority of the customers of AAC are male, which consists of 93% of the customers.")
+
+        # Age Distribution per Generation plot
+        st.markdown("<h4>Age Distribution</h4>", unsafe_allow_html=True)
+        generation_count = unique_holders['generation'].value_counts().to_frame(name="count").reset_index()
+        generation_count = generation_count[generation_count['count'] > 0]
+        generation_fig = px.bar(generation_count, x='generation', y='count', color='generation')
+        st.plotly_chart(generation_fig, key='generation_distri_plot')
+        st.write("Around 58% of the customers are Baby Boomers, which has the age range of 58 to 76. The youngest customer is 51 years old while the eldest is at 95 years old. The customer base of AAC has an average age of 67 years old.")
+
+        # City Distribution
+        st.markdown("<h4>City Distribution</h4>", unsafe_allow_html=True)
+        top_city_count = unique_holders['city'].value_counts()[0:5].to_frame(name="count").reset_index()
+        top_city_fig = px.bar(top_city_count, x='city', y='count', color='city')
+        st.plotly_chart(top_city_fig, key='top_city_distri_plot')
+        st.write("Out of the 94 unique customers, 5 live in San Fernando, 4 in Dasmarinas and Calapan, and 3 in Masbate and Pagadian. The rest live in other cities but the top city is San Fernando.")
+
+        # Categories based on number of transactions plot
+        st.markdown("<h4>Category based on number of transactions</h4>", unsafe_allow_html=True)
+        category_names = {"grocery_pos": "Physical Grocery",
+                  "shopping_pos": "Physical Shopping",
+                  "gas_transport": "Gas and Transportation",
+                  "kids_pets": "Kids and Pets",
+                  "home": "Home",
+                  "no_category": "No Category",
+                  "personal_care": "Personal Care",
+                  "food_dining": "Food and Dining",
+                  "entertainment": "Entertainment",
+                  "misc_pos": "Physical Miscellaneous",
+                  "health_fitness": "Health and Fitness",
+                  "shopping_net": "Online Shopping",
+                  "travel": "Travel",
+                  "misc_net": "Online Miscellaneous",
+                  "grocery_net": "Online Grocery"}
+        cat_trans = df_with_label.groupby(['category'])['trans_datetime'].count().sort_values(ascending=False).to_frame("count").reset_index()
+        cat_trans['category'] = cat_trans['category'].map(category_names)
+
+        cat_trans_fig = px.bar(cat_trans, x='count', y='category', color='category',
+                    labels={"count": "Number of Transactions"})
+        cat_trans_fig.update_layout(showlegend=False)
+        st.plotly_chart(cat_trans_fig, key='category_per_trans_fig')
+        st.write("We observe that the category that has the most number of transactions would be Physical Grocery. That is, grocery done face-to-face or physically.")
+
+        # Categories based on amount spent
+        st.markdown("<h4>Category based on the amount spent</h4>", unsafe_allow_html=True)
+        cat_sum = df_with_label.groupby('category')['amt'].sum().sort_values(ascending=False).to_frame("total").reset_index()
+        cat_sum['category'] = cat_sum['category'].map(category_names)
+
+        cat_sum_fig = px.bar(cat_sum, x='total', y='category', color='category',
+                    labels={"total": "Total Amount Spent"})
+        cat_sum_fig.update_layout(showlegend=False)
+        st.plotly_chart(cat_sum_fig, key='category_per_amt_fig')
+        st.write("Similarly, the Physical Grocery category has the highest amount spent. This may due to the number of transactions as notably, the more transactions on the category, the more amount is spent.")
+
+        # Summary of EDA
+        st.markdown("<h4>Summary of Findings</h4>", unsafe_allow_html=True)
+        st.write("Based on the plots, we can notice a few things.")
+        st.markdown(f"1. All of the customers of AAC are more on the older generation (50 and above).\n2. Majority of the customers are located in the Luzon area.\n3. Based on the overall transaction history, the customers tend to buy products physically or face-to-face.\n4. The top category in terms of number of transactions and amount spent is `Physical Grocery`. This may indicate that the customers tend to use their cards to buy grocery physically.")
 
     with st.expander("👥 **Clustering**", expanded=True):
         st.write("wow!")
